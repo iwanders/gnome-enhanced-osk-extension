d1fc606828e018baa61773d3fa5daa85087d078f
Ivor Wanders <ivor@iwanders.net>
2023-12-10 20:24:38 -0500

    Finish the last level.

--- util/render_keyboard.py
+++ util/render_keyboard.py
@@ -28,6 +28,7 @@ def ascii_level(level):
             l = k["strings"][0][0:w]
         if "iconName" in k:
             l = k["iconName"].replace("go-", "").replace("-symbolic", "").replace("keyboard-", "").replace("edit-", "")[0:w]
+        w = w - emoji_correction(l)
         return f"{l: ^{w}}"
 
     def row_length(r):
@@ -36,11 +37,26 @@ def ascii_level(level):
             return int(v)
         return v
 
+    def emoji_correction(l):
+        # Totally non robust lol
+        w = 0
+        if "🎉" in l:
+            w += 1
+        if "😀" in l:
+            w += 1
+        return w
+        
+    def str_len_accounting_for_emoji(s):
+        w = len(s)
+        w += emoji_correction(s)
+        return w
+        
+
     for r in level["rows"]:
         keys = [format_key(k) for k in r]
         row = "|".join(keys) + "|"
         width = row_length(r)
-        align = 120 - len(row)
+        align = 70 - str_len_accounting_for_emoji(row)
         data += f"{row}  {width: >{align}}"+ "\n"
     return data
     
